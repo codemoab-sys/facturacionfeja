723304aa17729436e8ae8c75ebecd58519de4ae8
moabcode <nombertoeduardo@gmail.com>
2026-07-01 01:31:38 -0500

    hardcodear API_DEFAULT_BASE_URL

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 $scriptDir = dirname($_SERVER['SCRIPT_NAME']);
 define('BASE_PATH', ($scriptDir === '/' || $scriptDir === '\\') ? '' : rtrim($scriptDir, '/'));
-define('API_DEFAULT_BASE_URL', '');
+define('API_DEFAULT_BASE_URL', 'https://apiprueba.moabcode.com/api/v1');
 
 error_reporting(0);
 ini_set('display_errors', '0');
